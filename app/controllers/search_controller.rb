b007baa373b989b463d39ef9a5e1077092ff7d94
CCLiff <CLCliff30@gmail.com>
2014-12-16 15:32:30 -0500

    fixes backend search function

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -2,10 +2,10 @@ class SearchController < ApplicationController
     get '/' do
       search = params['search']
       year = params['year']
-      year = nil if year == 'nil'
+      year = nil if year[0] == 'nil'
 
-        teams = Team.where('lower(name) LIKE ?', '%' + search.downcase + '%').year(year)
-        players = Player.joins(:teams).distinct.where('lower("firstName") LIKE ? OR lower("lastName") LIKE ?', '%' + search.downcase + '%', '%' + params['search'].downcase + '%').year(year)
+        teams = Team.where('lower(name) LIKE ?', '%' + search[0].downcase + '%').year(year).order(year: :desc)
+        players = Player.joins(:teams).distinct.where('lower("firstName") LIKE ? OR lower("lastName") LIKE ?', '%' + search[0].downcase + '%', '%' + search[0].downcase + '%').year(year).order(lastName: :asc)
 
         data = {teams: teams, players: players}
         data.to_json
