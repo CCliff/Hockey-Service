0b9985749c411871d479b7f7ae7fbadeac29fba0
CCLiff <CLCliff30@gmail.com>
2014-12-17 17:24:19 -0500

    small tweak to the player contoller (clean up)

--- app/controllers/player_controller.rb
+++ app/controllers/player_controller.rb
@@ -10,30 +10,7 @@ class PlayerController < ApplicationController
   get '/show' do
     content_type :json
     player = Player.find_by(playerID: params['id'])
-    seasons = player.scoring_seasons
-    # last_season = {}
-    # seasons.map do |season|
-
-    #   if last_season['year'] == season['year']
-    #     season.tmID = "VAR"
-    #     season.GP = last_season.GP + season.GP
-    #     season.G = last_season.G + season.G
-    #     season.A = last_season.A + season.A
-    #     season.Pts = last_season.Pts + season.Pts
-    #     season.PIM = last_season.PIM + season.PIM
-    #     season.PlusMinus = last_season.PlusMinus + season.PlusMinus
-    #     season.PPG = last_season.PPG + season.PPG
-    #     season.PPA = last_season.PPA + season.PPA
-    #     season.SHG = last_season.SHG + season.SHG
-    #     season.SHA = last_season.SHA + season.SHA
-    #     season.GWG = last_season.GWG + season.GWG
-    #     season.SOG = last_season.SOG + season.SOG
-    #   end
-
-    #   last_season = season
-    #   season
-
-    # end
+    seasons = player.scoring_seasons.order(year: :asc)
 
     data = {
       player: player,
