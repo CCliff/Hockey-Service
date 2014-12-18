80f6b15da7a742e639f87c731e5be7d1b89d817d
CCLiff <CLCliff30@gmail.com>
2014-12-18 17:04:48 -0500

    x

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -4,8 +4,8 @@ class SearchController < ApplicationController
       year = params['year']
       year = nil if year[0] == 'nil'
 
-        teams = Team.where('lower(name) LIKE ?', '%' + search[0].downcase + '%').year(year).order(year: :desc)
-        players = Player.joins(:teams).distinct.where('lower("firstName") LIKE ? OR lower("lastName") LIKE ?', '%' + search[0].downcase + '%', '%' + search[0].downcase + '%').year(year).order(lastName: :asc)
+        teams = Team.where('lower(name) LIKE ?', '%' + search[0].downcase + '%').year(year).order(year: :desc).limit(25)
+        players = Player.joins(:teams).distinct.where('lower("firstName") LIKE ? OR lower("lastName") LIKE ?', '%' + search[0].downcase + '%', '%' + search[0].downcase + '%').year(year).order(lastName: :asc).limit(25)
 
         data = {teams: teams, players: players}
         data.to_json
